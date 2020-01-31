24871fbac4d6bec9237fe3a61e0e180c48c44ec1
Coralie-BDY <coralie.bdy@gmail.com>
2020-01-31 12:17:34 +0100

    essai

---
+++ hotel/js/app.js
@@ -0,0 +1,14 @@
+console.log("coucou") ;
+
+//$(function(){
+//   setInterval(function(){
+//      $(".slideshow ul").animate({marginLeft:-350},800,function(){
+//         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+//      })
+//   }, 3500);
+//});
+//
+//      
+//
+//(jQuery);
+
